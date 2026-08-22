ca41226c58c9141fbc024cfed2742af13342e201
Marcus Comstedt <marcus@mc.pp.se>
2026-08-22 11:48:10 +0200

    sdhost: Fix broken brcm,force-pio DT property

--- drivers/mmc/host/bcm2835.c
+++ drivers/mmc/host/bcm2835.c
@@ -1489,16 +1489,20 @@ static int bcm2835_probe(struct platform_device *pdev)
 
 	host->dma_chan = NULL;
 	host->dma_desc = NULL;
-
-	host->dma_chan_rxtx = dma_request_chan(dev, "rx-tx");
-	if (IS_ERR(host->dma_chan_rxtx)) {
-		ret = PTR_ERR(host->dma_chan_rxtx);
+	
+	if (!allow_dma)
 		host->dma_chan_rxtx = NULL;
+	else {
+		host->dma_chan_rxtx = dma_request_chan(dev, "rx-tx");
+		if (IS_ERR(host->dma_chan_rxtx)) {
+			ret = PTR_ERR(host->dma_chan_rxtx);
+			host->dma_chan_rxtx = NULL;
 
-		if (ret == -EPROBE_DEFER)
-			goto err;
+			if (ret == -EPROBE_DEFER)
+				goto err;
 
-		/* Ignore errors to fall back to PIO mode */
+			/* Ignore errors to fall back to PIO mode */
+		}
 	}
 
 	fw = rpi_firmware_get(
